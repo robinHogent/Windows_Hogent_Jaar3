835455d9846dddcd9af7c592cf0283404677cdfb
robinHogent <robin.decock.y6890@student.hogent.be>
2018-10-18 12:04:43 +0200

    Update SCCM.ps1

--- Scripts/SCCM.ps1
+++ Scripts/SCCM.ps1
@@ -1,3 +1,5 @@
 Rename-computer -newname WDS  
 New-NetIPAddress -InterfaceAlias "Ethernet 2" -IPAddress "192.168.1.4" -AddressFamily IPv4 -PrefixLength 24 -DefaultGateway "192.168.1.1"
 Set-DnsClientServerAddress -InterfaceAlias "Ethernet 2" -ServerAddresses "192.168.1.1"
+wget http://download.microsoft.com/download/D/7/E/D7E22261-D0B3-4ED6-8151-5E002C7F823D/adkwinpeaddons/adkwinpesetup.exe -O adkwinpesetup.exe
+.\adkwinpesetup.exe
