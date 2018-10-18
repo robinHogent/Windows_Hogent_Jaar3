fb681d5ca8356d47bec5684c075b8b6a55536c28
robinHogent <robin.decock.y6890@student.hogent.be>
2018-10-18 14:19:21 +0200

    Update sql.ps1

--- Scripts/sql.ps1
+++ Scripts/sql.ps1
@@ -1,4 +1,4 @@
 Rename-computer -newname SQL  
 New-NetIPAddress -InterfaceAlias "Ethernet 2" -IPAddress "192.168.1.3" -AddressFamily IPv4 -PrefixLength 24 -DefaultGateWay 192.168.1.1
-Set-DnsClientServerAddress -InterfaceAlias Ethernet -ServerAddresses "192.2.1.1"
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet 2" -ServerAddresses "192.2.1.1"
 Restart-Computer -f
